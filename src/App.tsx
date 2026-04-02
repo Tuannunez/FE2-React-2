71aac71d40779665cc8a8b99cd21254d18da4a17
Tuannunez <tuantrungpham008@gmail.com>
2026-04-02 14:13:09 +0700

    thêm themmeContext

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,28 @@ import {Table } from "antd";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Login from "./components/Login";
+import { UserProvider } from "./context/UserContext";
+import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
   
   return (
-    <>
-    <Navbar />
-      {/* MAIN CONTENT */}
-      <div className="max-w-6xl mx-auto mt-10 px-4 text-center">
-        <h1 className="text-4xl font-bold mb-4">Chào mừng đến với WEB2091</h1>
-        <Header/>
-        <Login/>
-        <Table columns={[]} dataSource={[]} />
-      </div>
+    <UserProvider>
+      <ThemeProvider>
+        <>
+          <Navbar />
+          {/* MAIN CONTENT */}
+          <div className="max-w-6xl mx-auto mt-10 px-4 text-center">
+            <h1 className="text-4xl font-bold mb-4">Chào mừng đến với WEB2091</h1>
+            <Header/>
+            <Login/>
+            <Table columns={[]} dataSource={[]} />
+          </div>
 
-      <Toaster />
-    </>
+          <Toaster />
+        </>
+      </ThemeProvider>
+    </UserProvider>
   );
 }
 
---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,20 @@
+import { createContext, useState } from "react";
+
+type Theme = "light" | "dark";
+
+type ThemeContextType = {
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
+};
+
+export const ThemeContext = createContext<ThemeContextType | null>(null);
+
+export const ThemeProvider = ({ children }: { children: any }) => {
+  const [theme, setTheme] = useState<Theme>("light");
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
